c98a3a0646be78c2af28ebaee668e034631b6d7d
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-17 13:54:45 -0500

    todo app in react completed, No local storage && no comps

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -16,6 +16,23 @@ export default function App() {
     setNewItem("");
   }
 
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
   return (
     <>
       <form onSubmit={handelSubmit} className="new-item-form">
@@ -32,14 +49,24 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todo's"}
         {todos.map((todo) => {
           return (
             <>
               <li key={todo.id}>
                 <label>
-                  <input type="checkbox" checked={todo.completed} />
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                  />
                   {todo.title}
-                  <button className="btn btn-danger">DELETE</button>
+                  <button
+                    onClick={() => deleteTodo(todo.id)}
+                    className="btn btn-danger"
+                  >
+                    DELETE
+                  </button>
                 </label>
               </li>
             </>
