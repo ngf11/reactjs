921fba7f8f5a20afddc842dc8592929afa00e547
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-18 13:56:40 -0500

    react styles

--- reactSeries/tut01/src/App.jsx
+++ reactSeries/tut01/src/App.jsx
@@ -1,7 +1,6 @@
 import Headers from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
-import "./index.css";
 
 export default function App() {
   return (
--- reactSeries/tut01/src/Content.jsx
+++ reactSeries/tut01/src/Content.jsx
@@ -3,9 +3,27 @@ const Content = () => {
     const names = ["nico", "siggy", "didi", "dog"];
     return names[Math.floor(Math.random() * names.length)];
   };
+  const handelGreet = () => {
+    const greets = ["Hello", "Hola", "Bonjour", "こんにちは"];
+    return greets[Math.floor(Math.random() * greets.length)];
+  };
+  const handelClick = () => {
+    console.log("You got it");
+  };
+  const handelClickTwo = (name) => {
+    console.log(`${name} was here`);
+  };
+  const handelClickThree = (e) => {
+    console.log(e.target.innerText);
+  };
   return (
     <main>
-      <p>Hello {handelNameChange()}</p>
+      <p onDoubleClick={handelClick}>
+        {handelGreet()}, {handelNameChange()}
+      </p>
+      <button onClick={handelClick}>Click me</button>
+      <button onClick={() => handelClickTwo("nico")}>Click me</button>
+      <button onClick={(e) => handelClickThree(e)}>Click me</button>
     </main>
   );
 };
