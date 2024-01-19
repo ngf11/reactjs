ec75b29198978acd615e0a3adc24d9ccd39ca96e
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-19 14:49:02 -0500

    UI food app

--- JustTry/src/Journal.jsx
+++ JustTry/src/Journal.jsx
@@ -11,6 +11,19 @@ export function Journal() {
             rows="10"
           ></textarea>
         </div>
+        <div className="wins">
+          <label htmlFor="wins">Write three wins:</label>
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <p className="small-text">
+            Write down three wins you had today. It could be anything i.e: I
+            went for a walk, I worked-out, I ate a healty meal.
+          </p>
+        </div>
+        <div className="sub-bnt">
+          <button className="btn">Submmit</button>
+        </div>
       </form>
     </div>
   );
