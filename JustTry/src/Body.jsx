64e364b10fa3358a67e36ab99418f96926d96cd2
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-19 23:46:22 -0500

    UI food app

--- JustTry/src/Body.jsx
+++ JustTry/src/Body.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./Header";
 import { Journal } from "./Journal";
+import { Workout } from "./Workout";
 
 export function Body() {
   return (
     <div className="App">
       <Header />
       <Journal />
+      <Workout />
     </div>
   );
 }
--- JustTry/src/Workout.jsx
+++ JustTry/src/Workout.jsx
@@ -1,7 +1,26 @@
 export function Workout() {
   return (
-    <div className="card workout">
-      <div className="routine"> </div>
+    <div className="card-container workout ">
+      <p className="smaller-text titel">Workout of the Day</p>
+      <div className="card  ">
+        <div className="routine">
+          <p className="smaller-text">Back Squats</p>
+          <p className="smaller-text">Lunges</p>
+          <p className="smaller-text">Romanian Deadlift</p>
+          <p className="smaller-text">Hip Thruster</p>
+          <p className="smaller-text">hamstring Curls</p>
+        </div>
+        <div className="routine">
+          <p className="smaller-text">3 sets 10-15 reps</p>
+          <p className="smaller-text">3 sets 10-15 reps</p>
+          <p className="smaller-text">3 sets 10-15 reps</p>
+          <p className="smaller-text">3 sets 10-15 reps</p>
+          <p className="smaller-text">3 sets 10-15 reps</p>
+        </div>
+      </div>
+      <div className="sub-bnt">
+        <button className="btn">Submmit</button>
+      </div>
     </div>
   );
 }
