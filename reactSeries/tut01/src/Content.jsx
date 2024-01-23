69891c70e21d4343946bfe050cbb5c79e4008f9f
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-23 13:26:19 -0500

    checked boxed fixed

--- reactSeries/tut01/src/Content.jsx
+++ reactSeries/tut01/src/Content.jsx
@@ -9,23 +9,36 @@ const Content = () => {
       item: "Open pound of Beef",
     },
     {
-      id: 1,
+      id: 2,
       checked: false,
       item: "Item 2",
     },
     {
-      id: 1,
+      id: 3,
       checked: false,
       item: "Item 3",
     },
   ]);
+
+  const handelCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
 
   return (
     <main>
       <ul>
         {items.map((item) => (
           <li className="item" key={item.id}>
-            <input type="checkbox" checked={item.checked} />
+            <input
+              type="checkbox"
+              onChange={() => {
+                handelCheck(item.id);
+              }}
+              checked={item.checked}
+            />
             <label>{item.item}</label>
             <FaTrashAlt role="button" tabIndex={0} />
           </li>
