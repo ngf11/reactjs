fd6b25412608b725c7bbb5c7674a681383ead794
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-22 21:53:07 -0500

    List  Items setitems

--- reactSeries/tut01/src/Content.jsx
+++ reactSeries/tut01/src/Content.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
   const [items, setItems] = useState([
@@ -26,6 +27,7 @@ const Content = () => {
           <li className="item" key={item.id}>
             <input type="checkbox" checked={item.checked} />
             <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex={0} />
           </li>
         ))}
       </ul>
