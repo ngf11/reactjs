e2e5772c2acf031920d39efef44a3b9f26e97ec9
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-24 14:36:41 -0500

    React Props

--- reactSeries/tut01/src/App.jsx
+++ reactSeries/tut01/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 export default function App() {
   return (
     <div className="App">
-      <Headers />
+      <Headers title="Groceries List" />
       <Content />
       <Footer />
     </div>
--- reactSeries/tut01/src/Header.jsx
+++ reactSeries/tut01/src/Header.jsx
@@ -1,9 +1,12 @@
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{title}</h1>
     </header>
   );
+};
+Header.defaultProps = {
+  title: "Default Titel",
 };
 
 export default Header;
