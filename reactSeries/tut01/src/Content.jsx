b514d9e4761bd769416123767004a8017cb02833
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-23 22:11:57 -0500

    Delete button

--- reactSeries/tut01/src/Content.jsx
+++ reactSeries/tut01/src/Content.jsx
@@ -36,30 +36,34 @@ const Content = () => {
 
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              onChange={() => {
-                handelCheck(item.id);
-              }}
-              checked={item.checked}
-            />
-            <label
-              style={item.checked ? { textDecoration: "line-through" } : null}
-              onDoubleClick={() => handelCheck(item.id)}
-            >
-              {item.item}
-            </label>
-            <FaTrashAlt
-              onClick={() => handelDelete(item.id)}
-              role="button"
-              tabIndex={0}
-            />
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => {
+                  handelCheck(item.id);
+                }}
+                checked={item.checked}
+              />
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handelCheck(item.id)}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt
+                onClick={() => handelDelete(item.id)}
+                role="button"
+                tabIndex={0}
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your List is Empty</p>
+      )}
     </main>
   );
 };
