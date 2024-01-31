a8e215ae479c9f39965ad982f39c13068a0d2ea0
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-30 21:44:42 -0500

    Form Add Item

--- reactSeries/tut01/src/AddItem.jsx
+++ reactSeries/tut01/src/AddItem.jsx
@@ -1,7 +1,7 @@
 import { FaPlus } from "react-icons/fa";
-const AddItem = () => {
+const AddItem = ({ newItem, setNewItems, handelSubmit }) => {
   return (
-    <form className="addForm">
+    <form className="addForm" onSubmit={handelSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
@@ -9,6 +9,8 @@ const AddItem = () => {
         type="text"
         placeholder="Add Item"
         required
+        value={newItem}
+        onChange={(e) => setNewItems(e.target.value)}
       />
       <button type="submit" aria-label="Add Item">
         <FaPlus />
--- reactSeries/tut01/src/App.jsx
+++ reactSeries/tut01/src/App.jsx
@@ -38,6 +38,7 @@ export default function App() {
   };
 
   const handelSubmit = (e) => {
+    e.preventDefault();
     console.log("Submitted ");
   };
   return (
